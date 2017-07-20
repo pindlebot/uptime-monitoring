21c65aa1d56e4d6b85512d022de5a67c7da346d3
Ben <bgardner620@gmail.com>
2017-07-20 12:49:23 -0400

    new

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  twilioNumber: process.env.TWILIO_NUMBER,
+  recipientNumber: process.env.RECIPIENT_NUMBER
+}
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const schedule = require('node-schedule')
 const twilio = require('twilio')
 require('isomorphic-fetch')
+if(process.env.NODE_ENV === 'dev') { require('env2')('.env') }
+const config = require('./config')
 
 const sites = ['http://www.example.com']
+var twilioClient = new twilio(config.accountSid, config.authToken);
 
-const accountSid = ''; 
-const authToken = '';   
-var twilioClient = new twilio(accountSid, authToken);
-
-const sendMessage = async ({site, to, from}) => {
-  var body = `${site} is down!`
-  return await twilioClient.messages.create({body, to, from}) 
+const sendMessage = async (site) => {
+  return await twilioClient.messages.create({
+    body: `${site} is down!`,
+    to: config.recipientNumber, 
+    from: config.twilioNumber
+  }) 
 }
 
 const run = async () => {
@@ -18,11 +20,14 @@ const run = async () => {
     var resp = await fetch(sites[i]) 
     if(resp.status >= 200 && resp.status < 300) {
       // Site is OK
+      console.log("Site is OK")
     } else {
       // Site is down; send an SMS
-      var resp = await sendMessage({site: sites[i], to: 'SENDER_#', from: 'RECIPIENT_#'})
+      var resp = await sendMessage(sites[i])
+      console.log(resp)
     }
   }
 }
 
-schedule.scheduleJob('*/1 * * * *', () => { run() })
+//schedule.scheduleJob('*/1 * * * *', () => { run() })
+run()
